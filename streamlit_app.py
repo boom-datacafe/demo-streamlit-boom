c543e85dc9bc2bc4e5060582a7cf4bf273fed6ce
boom-datacafe <111261717+boom-datacafe@users.noreply.github.com>
2024-10-12 03:49:41 +0000

    # Please enter the commit message for your changes. Lines starting # with '#' will be ignored, and an empty message aborts the commit. # # On branch main # Your branch is up to date with 'origin/main'. # # Changes to be committed: #	modified:   requirements.txt #	modified:   streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,34 @@
 import streamlit as st
+import google.generativeai as genai
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+st.title("🕵🏻‍♂️ My Chatbot app")
+st.subheader("Conversation")
+st.write("DEMO FIRST APP.")
+
+
+gemini_api_key = st.text_input("Gemini API Key: ", placeholder="Type your API Key here ...")
+
+if "chat_history" not in st.session_state:
+    st.session_state.chat_history = []
+
+for message in st.session_state.chat_history:
+    with st.chat_message("user"):
+        st.markdown(message)
+
+if prompt := st.chat_input("Type your message here ..."):
+    st.session_state.chat_history.append(prompt)
+    st.chat_message("user").markdown(prompt)
+
+st.write(st.session_state.chat_history)
+
+# if user_input :=  st.text_input(label="You: ", placeholder="Type your message here..."):
+#     st.session_state.chat_history.append(user_input) 
+
+
+
+
+
+
+
+
+
